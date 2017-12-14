e7f0ae19924bc2d695196f4979991f8d442ff107
Bas Wegh <bas.wegh@kit.edu>
2017-12-14 09:45:34 +0100

    ensure the current code will be used

--- utils/compile.sh
+++ utils/compile.sh
@@ -3,6 +3,7 @@
 GO=`which go`
 PATH_TO_SCRIPT=`readlink -f ${0}`
 PATH_TO_FOLDER=`dirname "$PATH_TO_SCRIPT"`
+SOURCE_FOLDER="_build/src/github.com/watts-kit/"
 
 if [ "x$GO" == "x" ]; then
     echo "go missing, please install go 1.5 or newer"
@@ -13,7 +14,7 @@ VERSION=`go version`
 
 cd "${PATH_TO_FOLDER}/.."
 rm -rf _build/
-mkdir -p _build/src/github.com/watts-kit/
+mkdir -p $SOURCE_FOLDER
 mkdir -p _build/bin
 
 GOPATH=`cd _build && pwd -P`
@@ -21,11 +22,13 @@ echo " "
 echo "running the build with '$VERSION', please include in issue reports"
 echo " using GOPATH=${GOPATH} "
 export "GOPATH=${GOPATH}"
-cd _build/src/github.com/watts-kit/
+cd $SOURCE_FOLDER
 git clone https://github.com/watts-kit/watts_plugin_info.git
-cd watts_plugin_info
+cd "${PATH_TO_FOLDER}/.."
+echo "updating with local version"
+cp *.go "$SOURCE_FOLDER/watts_plugin_info"
+cd "$SOURCE_FOLDER/watts_plugin_info"
 go get -v -u
 go build -o watts_plugin_info main.go
 cd "${PATH_TO_FOLDER}/.."
-cp _build/bin/watts_plugin_info .
 echo "done"
